bbe67782718c9dd050e0ba0bac367502f9ec1cf9
Md Ibrahim Adham Mesu <72233105+mesu-ai@users.noreply.github.com>
2023-11-27 11:58:39 +0000

    Update likes

--- src/app/post/LikeButton.jsx
+++ src/app/post/LikeButton.jsx
@@ -1,16 +1,30 @@
 'use client'
 
+import { useSession } from 'next-auth/react';
 import React from 'react';
 
-const LikeButton = ({ postId}) => {
-  
-const handleLike = (id) => {
-  console.log(id);
-}
+const LikeButton = ({ postId }) => {
+  const { data: session } = useSession();
+
+  const handleLike = async (id) => {
+    console.log(id);
+
+    const res = await fetch(`http://localhost:3000/api/post/${id}/like`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${session?.accessToken}`
+      },
+      body: JSON.stringify({postId:id}),
+    });
+
+    
+
+  }
 
   return (
-    <button onClick={()=>handleLike(postId)}
-     className="my-3 px-4 py-1.5 bg-blue-500 text-white rounded-full ring-2 hover:ring-4 ring-blue-800 ring-inset" type="button">Like</button>
+    <button onClick={() => handleLike(postId)}
+      className="my-3 px-4 py-1.5 bg-blue-500 text-white rounded-full ring-2 hover:ring-4 ring-blue-800 ring-inset" type="button">Like</button>
   );
 };
 
